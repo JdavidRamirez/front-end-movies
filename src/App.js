7dbf909d4435f124ec9c6493d34c03a0cacd2281
JdavidRamirez <entropiadeluniverso@gmail.com>
2021-04-09 15:33:39 -0500

    first commit front-end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import  NavBar  from './Components/NavBar';
+import Table from './Components/Table'
 
 function App() {
+
+  const titulo = "Peliculas IUdigital"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <NavBar titulo={titulo}/>
+      <Table />
+       
+  
     </div>
   );
 }
